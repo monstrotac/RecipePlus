48ea0f91cb34451771c31f9bda7ec81e04cc701a
NoahPM <NoahPlourdeM@hotmail.com>
2022-04-05 17:02:32 -0400

    Added Recipe and Ingredient

---
+++ app/src/main/java/uqac/dim/recipeplus/Ingredient.java
@@ -0,0 +1,47 @@
+package uqac.dim.recipeplus;
+
+public class Ingredient {
+    private int id;
+    private String name;
+    private String desc;
+    private float avgPrice;
+
+    public Ingredient(int id, String name,String desc,float avgPrice){
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this. avgPrice = avgPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public float getAvgPrice() {
+        return avgPrice;
+    }
+
+    public void setAvgPrice(float avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+}
---
+++ app/src/main/java/uqac/dim/recipeplus/Recipe.java
@@ -0,0 +1,49 @@
+package uqac.dim.recipeplus;
+
+import java.util.List;
+
+public class Recipe {
+    private int id;
+    private String name;
+    private String desc;
+    private List<Ingredient> ingredients;
+
+    public Recipe(int id,String name,String desc,List<Ingredient> ingredients){
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.ingredients = ingredients;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+}
