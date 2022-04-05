a86aa841d57a13376ba2b8e9d74a73d83634671a
NoahPM <NoahPlourdeM@hotmail.com>
2022-04-05 16:47:43 -0400

    Added all the pages, need more work tho

--- app/src/main/java/uqac/dim/recipeplus/MainActivity.java
+++ app/src/main/java/uqac/dim/recipeplus/MainActivity.java
@@ -3,6 +3,7 @@ package uqac.dim.recipeplus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View v){
+
+    }
+
+    public void Register(View v){
+
+    }
+
+    public  void ContinueGuest(View v){
+
     }
 }
---
+++ app/src/main/java/uqac/dim/recipeplus/RecipeActivity.java
@@ -0,0 +1,6 @@
+package uqac.dim.recipeplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeActivity extends AppCompatActivity {
+}
